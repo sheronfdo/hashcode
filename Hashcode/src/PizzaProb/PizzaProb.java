fa759c473ecf95291cb1d7121c4a0a6e412faec2
sheronfdo <jamithsheron5@gmail.com>
2022-02-05 15:25:21 +0530

    formatted

--- Hashcode/src/PizzaProb/PizzaProb.java
+++ Hashcode/src/PizzaProb/PizzaProb.java
@@ -17,47 +17,44 @@ import java.util.ArrayList;
  * @author Jamit
  */
 public class PizzaProb {
+
     private int clientsCount = 0;
     private int likePreferences = 0;
     private int dislikePreferences = 0;
     private String[] likeArray;
     private String dislikeArray[];
     static File myObj;
+
     public static void main(String[] args) {
         openFile();
     }
-    private static void openFile(){
+
+    private static void openFile() {
         try {
             myObj = new File("a_an_example.in.txt");
             Scanner myReader = new Scanner(myObj);
-            int n= 0;
+            int n = 0;
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 //int i = Integer.parseInt(data);
                 n++;
-                if(n==1)
-                {
-                   //int clientsCount = Integer.parseInt(data);
-                   System.out.println("Number of customers "+data);
-                }
-                else if(n%2==0)
-                {
+                if (n == 1) {
+                    //int clientsCount = Integer.parseInt(data);
+                    System.out.println("Number of customers " + data);
+                } else if (n % 2 == 0) {
                     String likeArray = new String();
-                    
-                    likeArray = data;                   
-                    
-                
-                  System.out.println("like array "+likeArray);
-                  
-                }
-                else if(n%2==1)
-                {
+
+                    likeArray = data;
+
+                    System.out.println("like array " + likeArray);
+
+                } else if (n % 2 == 1) {
                     String dislikeArray = new String();
-                    
+
                     dislikeArray = data;
-                    
-                   System.out.println("Dislike array "+dislikeArray);
-                } 
+
+                    System.out.println("Dislike array " + dislikeArray);
+                }
                 //System.out.println(data);
             }
             myReader.close();
@@ -65,5 +62,5 @@ public class PizzaProb {
             Logger.getLogger(PizzaProb.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
