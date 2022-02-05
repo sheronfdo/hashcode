d433020bb56117752538be353cf019398fe44735
Anjelo <benjunior711@gmail.com>
2022-02-05 15:20:30 +0530

    Create Array

--- Hashcode/src/PizzaProb/PizzaProb.java
+++ Hashcode/src/PizzaProb/PizzaProb.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.ArrayList;
 
 /**
  *
@@ -19,25 +20,50 @@ public class PizzaProb {
     private int clientsCount = 0;
     private int likePreferences = 0;
     private int dislikePreferences = 0;
-    private String likeArray[];
+    private String[] likeArray;
     private String dislikeArray[];
     static File myObj;
-    
     public static void main(String[] args) {
         openFile();
     }
-    
     private static void openFile(){
         try {
             myObj = new File("a_an_example.in.txt");
             Scanner myReader = new Scanner(myObj);
+            int n= 0;
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
+                //int i = Integer.parseInt(data);
+                n++;
+                if(n==1)
+                {
+                   //int clientsCount = Integer.parseInt(data);
+                   System.out.println("Number of customers "+data);
+                }
+                else if(n%2==0)
+                {
+                    String likeArray = new String();
+                    
+                    likeArray = data;                   
+                    
+                
+                  System.out.println("like array "+likeArray);
+                  
+                }
+                else if(n%2==1)
+                {
+                    String dislikeArray = new String();
+                    
+                    dislikeArray = data;
+                    
+                   System.out.println("Dislike array "+dislikeArray);
+                } 
+                //System.out.println(data);
             }
             myReader.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(PizzaProb.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
 }
