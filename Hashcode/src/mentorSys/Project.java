65326dd5240b22922275657ff47ac8f1fc4edb09
sheronfdo <jamithsheron5@gmail.com>
2022-02-25 02:06:34 +0530

    modified project model

--- Hashcode/src/mentorSys/Project.java
+++ Hashcode/src/mentorSys/Project.java
@@ -8,28 +8,68 @@ package mentorSys;
 import java.util.ArrayList;
 
 public class Project {
-      String pname;
-      ArrayList<SkillAndLevel> skillandlevel = new ArrayList<SkillAndLevel>();
 
-    public Project() {        
+    String projectName;
+    int numberOfDays;
+    int scoreAward;
+    int bestBefore;
+    int numberOfRole;
+    ArrayList<SkillAndLevel> roleandlevel = new ArrayList<SkillAndLevel>();
+
+    public Project(String projectName, int numberOfDays, int scoreAward, int bestBefore, ArrayList<SkillAndLevel> skillandlevel) {
+        this.projectName = projectName;
+        this.numberOfDays = numberOfDays;
+        this.scoreAward = scoreAward;
+        this.bestBefore = bestBefore;
+        this.roleandlevel = skillandlevel;
     }
 
-    public String getPName() {
-        return pname;
+    public String getProjectName() {
+        return projectName;
     }
 
-    public void setPname(String pname) {
-        this.pname = pname;
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
     }
-    
+
+    public int getNumberOfDays() {
+        return numberOfDays;
+    }
+
+    public void setNumberOfDays(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+
+    public int getScoreAward() {
+        return scoreAward;
+    }
+
+    public void setScoreAward(int scoreAward) {
+        this.scoreAward = scoreAward;
+    }
+
+    public int getBestBefore() {
+        return bestBefore;
+    }
+
+    public void setBestBefore(int bestBefore) {
+        this.bestBefore = bestBefore;
+    }
+
+    public int getNumberOfRole() {
+        return numberOfRole;
+    }
+
+    public void setNumberOfRole(int numberOfRole) {
+        this.numberOfRole = numberOfRole;
+    }
+
     public ArrayList<SkillAndLevel> getSkillandlevel() {
-        return skillandlevel;
+        return roleandlevel;
     }
-    
-    
+
     public void setSkillandlevel(ArrayList<SkillAndLevel> skillandlevel) {
-        this.skillandlevel = skillandlevel;
+        this.roleandlevel = skillandlevel;
     }
     
-      
 }
